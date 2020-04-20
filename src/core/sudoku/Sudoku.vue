efa1c00485fb7796fd033c79f119dd159064ef2e
iRuxu <rx6@qq.com>
2020-04-20 19:47:02 +0800

    sudoku init

---
+++ src/core/sudoku/Sudoku.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="app">
+        <Header />
+        1111
+        <Breadcrumb APP_NAME="九宫格计算器" />
+        <Sidebar />
+
+        <main class="c-main p-app-timeline">
+
+            <div class="m-sudoku">
+                应用部分
+            </div>
+
+            <aside class="c-sidebar-right c-sidebar">
+                右侧边栏，历史存档等
+            </aside>
+        </main>
+    </div>
+</template>
+
+<script>
+import Breadcrumb from "@/components/Breadcrumb.vue";
+import Sidebar from "@/components/Sidebar.vue";
+const { LuaReader, LuaData } = require("lua4fe");
+
+export default {
+    name: "App",
+    data: function() {
+        return {};
+    },
+    computed: {},
+    methods: {},
+    filters: {},
+    mounted: function() {
+        const luap = new LuaReader();
+
+        luap.then((it) => {
+            it.watch("test", function(data) {
+                let result = data.toJSON();
+                console.log(result);
+            });
+        }).catch((err) => {
+            console.error(err);
+            console.log("not support 浏览器不支持");
+        });
+    },
+    components: {
+        Breadcrumb,
+        Sidebar,
+    },
+};
+</script>
+
+<style lang="less"></style>
---
+++ src/core/sudoku/sudoku.js
@@ -0,0 +1,20 @@
+Vue.config.productionTip = false;
+
+// import Vue from "vue";
+// import ElementUI from "element-ui";
+// import "element-ui/lib/theme-chalk/index.css";
+// Vue.use(ElementUI);
+
+import JX3BOX_UI from '@jx3box/jx3box-common/vue'
+Vue.use(JX3BOX_UI);
+
+// import router from "../router";
+// import store from "../store";
+
+import Sudoku from "./Sudoku.vue";
+
+new Vue({
+    // router,
+    // store,
+    render: (h) => h(Sudoku),
+}).$mount("#app");
