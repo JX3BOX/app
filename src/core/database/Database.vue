0efb6cc2e6935444639074792eb543e7aad8e7ea
iRuxu <rx6@qq.com>
2020-12-25 17:33:13 +0800

    update

--- src/core/database/Database.vue
+++ src/core/database/Database.vue
@@ -1049,6 +1049,7 @@ export default {
                 });
         },
         search: function() {
+            this.page = 1
             this.getData();
         },
         onCopy: function(val) {
