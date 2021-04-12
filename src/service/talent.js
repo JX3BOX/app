dd7691f406be0e7d49bb98b22bfc83611494766b
iRuxu <rx6@qq.com>
2021-04-13 01:41:46 +0800

    fix

--- src/service/talent.js
+++ src/service/talent.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { __ossRoot,__ossMirror } from "@jx3box/jx3box-common/data/jx3box.json";
 
 function getTalentVersions() {
-    return axios.get(__ossMirror + "data/qixue/index.json");
+    return axios.get(__ossRoot + "data/qixue/index.json");
 }
 
 export {getTalentVersions};
